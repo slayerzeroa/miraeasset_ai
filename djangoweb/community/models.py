e43fcd57a8db653de41e2d9f3c17b5c70d3773e7
hyojeong <dbsgywjd@cau.ac.kr>
2024-07-25 20:20:23 +0900

    add community

---
+++ djangoweb/community/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Board(models.Model):
+    author = models.CharField(max_length=10, null=False)
+    title = models.CharField(max_length=100, null=False)
+    content = models.TextField(null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
---
+++ djangoweb/community/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render
+
+from .models import *
+def community(request):
+    boards = {'boards': Board.objects.all()}
+    return render(request, 'list.html', boards)
+
+def post(request):
+    if request.method == "POST":
+        author = request.POST['author']
+        title = request.POST['title']
+        content = request.POST['content']
+        board = Board(author=author, title=title, content=content)
+        board.save()
+        return HttpResponseRedirect('/community/')
+    else:
+        return render(request, 'post.html')
+
+def detail(request, id):
+    try:
+        board = Board.objects.get(pk=id)
+    except Board.DoesNotExist:
+        raise Http404("Does not exist!")
+    return render(request, 'detail.html', {'board': board})
