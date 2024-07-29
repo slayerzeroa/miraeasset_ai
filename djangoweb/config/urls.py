d512c5a82387f11c46aec3b2d3468287a8970b89
slayerzeroa <slayerzeroa@naver.com>
2024-07-29 11:49:23 +0900

    fix: url patterns, contatiner

--- djangoweb/config/urls.py
+++ djangoweb/config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from config.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='main'),
@@ -11,3 +13,6 @@ urlpatterns = [
     path("community/", include("community.urls"), name='community'),
     path("pbreport/", include("mypage.urls"), name='pbreport'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
